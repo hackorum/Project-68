40b2fc040690b30036195c5ad5c8b26d7024a017
hackorum <neilsabde@gmail.com>
2020-12-22 19:43:48 +0530

    added files

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { createAppContainer } from "react-navigation";
+import FacebookScreen from "./screens/FacebookScreen";
+import InstagramScreen from "./screens/InstagramScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Container />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const navigator = createBottomTabNavigator({
+  Facebook: FacebookScreen,
+  Instagram: InstagramScreen,
 });
+
+const Container = createAppContainer(navigator);
